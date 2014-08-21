0f2a493cf03ece56a3a96cf9abd94795567809ba
GabrielAguilera <gabrieag@umich.edu>
2014-08-21 00:15:21 -0400

    Added action menu item, added dialog, and resources.

--- MusicWallpaper/src/com/aguilera/gabriel/musicwallpaper/MainActivity.java
+++ MusicWallpaper/src/com/aguilera/gabriel/musicwallpaper/MainActivity.java
@@ -7,6 +7,7 @@ import java.io.IOException;
 
 import android.app.Activity;
 import android.app.ActionBar;
+import android.app.Dialog;
 import android.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -14,6 +15,8 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
+import android.widget.TextView;
 import android.widget.Toast;
 import android.os.Build;
 import android.app.WallpaperManager;
@@ -27,102 +30,114 @@ import android.graphics.drawable.Drawable;
 
 public class MainActivity extends Activity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        
-        //android wallpaper manager
-        WallpaperManager myWallpaperManager = WallpaperManager.getInstance(getApplicationContext());
-        String originalWallpaperFileName = "original_wallpaper.png";
-        FileInputStream fis;
-        try{
-        	fis = openFileInput(originalWallpaperFileName);
-        	byte[] input = new byte[fis.available()];
-        	while(fis.read(input) != -1){}
-        	myWallpaperManager.clear();
-        	Bitmap bmp;
-        	bmp = BitmapFactory.decodeByteArray(input, 0, input.length);
-        	myWallpaperManager.setBitmap(bmp);
-        	
-        	Toast.makeText(getApplicationContext(), "Your old wallpaper was successfully opened!", Toast.LENGTH_SHORT).show();;
-        	fis.close();
-        	return;
-        }
-        catch(IOException e) {
-        	//fis.close();
-        	e.printStackTrace();
-        }
-        
-        
-        
-        Drawable originalWallpaper = myWallpaperManager.getDrawable(); //Gets current wallpaper
-        Bitmap originalWallpaperBitmap = ((BitmapDrawable)originalWallpaper).getBitmap(); //Converts it to a bitmap
-        ByteArrayOutputStream stream = new ByteArrayOutputStream(); //Prepares a byte array
-        originalWallpaperBitmap.compress(Bitmap.CompressFormat.PNG, 100, stream); // compress it
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
 
-        byte[] bitmapData = stream.toByteArray(); // make into a byte array
-        FileOutputStream fos;
-        try {
-        	fos = openFileOutput(originalWallpaperFileName, Context.MODE_PRIVATE); //Open stream
-        	fos.write(bitmapData); //Save the file.
-        	Toast.makeText(getApplicationContext(), "Original wallpaper successfully saved!", Toast.LENGTH_SHORT).show();
-        	fos.close();
-        }
-        catch(IOException e){
-        	e.printStackTrace();
-        }
-        
-        try 
-        {
-        	//set wallpaper picture from resource here
-        	myWallpaperManager.setResource(R.drawable.mbdtf);
-        	Toast.makeText(getApplicationContext(), "Wallpaper change was successful!", Toast.LENGTH_SHORT).show();
-        }
-        catch(IOException e){
-        	Toast.makeText(getApplicationContext(), "Unsuccessful. :(", Toast.LENGTH_SHORT).show();
-        }
-        
-//        if (savedInstanceState == null) {
-//            getFragmentManager().beginTransaction()
-//                    .add(R.id.container, new PlaceholderFragment())
-//                    .commit();
-//        }
-    }
+		//android wallpaper manager
+		WallpaperManager myWallpaperManager = WallpaperManager.getInstance(getApplicationContext());
+		String originalWallpaperFileName = "original_wallpaper.png";
+		FileInputStream fis;
+		try{
+			fis = openFileInput(originalWallpaperFileName);
+			byte[] input = new byte[fis.available()];
+			while(fis.read(input) != -1){}
+			myWallpaperManager.clear();
+			Bitmap bmp;
+			bmp = BitmapFactory.decodeByteArray(input, 0, input.length);
+			myWallpaperManager.setBitmap(bmp);
+
+			Toast.makeText(getApplicationContext(), "Your old wallpaper was successfully opened!", Toast.LENGTH_SHORT).show();;
+			fis.close();
+			return;
+		}
+		catch(IOException e) {
+			//fis.close();
+			e.printStackTrace();
+		}
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
+		Drawable originalWallpaper = myWallpaperManager.getDrawable(); //Gets current wallpaper
+		Bitmap originalWallpaperBitmap = ((BitmapDrawable)originalWallpaper).getBitmap(); //Converts it to a bitmap
+		ByteArrayOutputStream stream = new ByteArrayOutputStream(); //Prepares a byte array
+		originalWallpaperBitmap.compress(Bitmap.CompressFormat.PNG, 100, stream); // compress it
 
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-//    public static class PlaceholderFragment extends Fragment {
-//
-//        public PlaceholderFragment() {
-//        }
-//
-//        @Override
-//        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-//                Bundle savedInstanceState) {
-//            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-//            return rootView;
-//        }
-//    }
+		byte[] bitmapData = stream.toByteArray(); // make into a byte array
+		FileOutputStream fos;
+		try {
+			fos = openFileOutput(originalWallpaperFileName, Context.MODE_PRIVATE); //Open stream
+			fos.write(bitmapData); //Save the file.
+			Toast.makeText(getApplicationContext(), "Original wallpaper successfully saved!", Toast.LENGTH_SHORT).show();
+			fos.close();
+		}
+		catch(IOException e){
+			e.printStackTrace();
+		}
+
+		try 
+		{
+			//set wallpaper picture from resource here
+			myWallpaperManager.setResource(R.drawable.mbdtf);
+			Toast.makeText(getApplicationContext(), "Wallpaper change was successful!", Toast.LENGTH_SHORT).show();
+		}
+		catch(IOException e){
+			Toast.makeText(getApplicationContext(), "Unsuccessful. :(", Toast.LENGTH_SHORT).show();
+		}
+
+		//        if (savedInstanceState == null) {
+		//            getFragmentManager().beginTransaction()
+		//                    .add(R.id.container, new PlaceholderFragment())
+		//                    .commit();
+		//        }
+	}
+
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		switch(item.getItemId()){
+
+		case R.id.action_about:
+		//TextView updatedAboutVersion = (TextView)findViewByID(R.id.aboutVersion);
+			//String updatedVersionString = getString(R.string.versionLabel) + getString(R.string.versionNumber);
+			//((TextView)findViewById(R.id.aboutVersion)).setText(updatedVersionString);
+			Dialog dialog = new Dialog(this); //Can't use getApplicationContext
+				dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+				dialog.setContentView(R.layout.action_about);
+				dialog.setCancelable(true);
+				dialog.show();
+				return true;
+
+		case R.id.action_settings:
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+
+	/**
+	 * A placeholder fragment containing a simple view.
+	 */
+	//    public static class PlaceholderFragment extends Fragment {
+	//
+	//        public PlaceholderFragment() {
+	//        }
+	//
+	//        @Override
+	//        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+	//                Bundle savedInstanceState) {
+	//            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+	//            return rootView;
+	//        }
+	//    }
 }
